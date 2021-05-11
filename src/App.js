ac8eeff2e021c8b79f8e9089baac0a1d46cc7b13
niki-sal <ne.salehi@gmail.com>
2021-05-11 14:00:37 -0700

    Number dashboard

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Widget from './components/Widget';
+import NumberWidgetContainer from './components/NumberWidgetContainer';
 
 function App() {
   return (
     <div>
       <h2>Welcome to databoard</h2>
       <Widget/>
+      <NumberWidgetContainer href="http://localhost:3001/tickets/open" heading="Open Ticket Total" />
     </div>
   );
 }
---
+++ src/components/NumberWidget.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NumberWidget extends Component {
+    render() {
+        return (
+            <div>
+                numberwidget
+            </div>
+        );
+    }
+}
+
+export default NumberWidget;
