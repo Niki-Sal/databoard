0272596d3df7ba28245a6df49d06dcbe5b9a4849
niki-sal <ne.salehi@gmail.com>
2021-05-11 16:42:12 -0700

    Add list dashboard

--- server.js
+++ server.js
@@ -21,6 +21,43 @@ app.get('/tickets/*', function (req, res) {
     })
 });
 
+app.get('/stats/top', function (req, res) {
+    res.json([
+        {
+            label: "Lauren",
+            value: Math.floor(Math.random() * 5) + 26
+        },
+        {
+            label: "Dave",
+            value: Math.floor(Math.random() * 5) + 13
+        },
+        {
+            label: "John",
+            value: Math.floor(Math.random() * 5) + 18
+        },
+        {
+            label: "Gregg",
+            value: Math.floor(Math.random() * 5) + 19
+        },
+        {
+            label: "Matt",
+            value: Math.floor(Math.random() * 5) + 4
+        },
+        {
+            label: "Jaral",
+            value: Math.floor(Math.random() * 5) + 12
+        },
+        {
+            label: "Bridget",
+            value: Math.floor(Math.random() * 5) + 18
+        },
+        {
+            label: "Rob",
+            value: Math.floor(Math.random() * 5) + 7
+        }
+    ]);    
+});
+
 app.listen(4000, function () {
     console.log('Data being served from http://localhost:4000');
 });
--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Widget from './components/Widget';
+import ListWidgetContainer from './components/ListWidgetContainer';
 import NumberWidgetContainer from './components/NumberWidgetContainer';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div>
       <h2>Welcome to databoard</h2>
       <Widget/>
+      <ListWidgetContainer href="http://localhost:4000/stats/top" heading="Top Ticket Answerers" rowspan={3} />
       <NumberWidgetContainer href="http://localhost:4000/tickets/open" heading="Open Ticket Total" />
     </div>
   );
